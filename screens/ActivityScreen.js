5a1826447609cf3cb2d2b573f662b2e4e658ccbe
Andreas Skifjeld <andrefjeld@gmail.com>
2018-10-15 04:47:52 +0200

    closes #1 - Implemented drawer and stack menu. Also changed to a navigator <-> screen structure

---
+++ screens/ActivityScreen.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, List, ListItem, CheckBox } from 'native-base';
+import Todo from '../components/Todo/Todo';
+
+/*
+  TODO:
+  Add loading and storing off Activities
+  Add delete, add and edit to Activities
+  Add testing
+  +++
+*/
+
+class ActivityScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completedTasks: 7,
+      totalTasks: 12,
+      todos: [{name: null, checked: null}, {name: "Label 2", checked: true}, {name: "Label 3", checked: false}, {name: "Label 4", checked: true}, {name: "Label 5 ", checked: true}]
+    };
+  }
+
+  setTodos(newTodo) {
+    let tempTodos = this.state.todos.slice();
+    tempTodos.push(newTodo);
+    this.setState({
+      todos: tempTodos
+    });
+  }
+
+  updateTaskCount() {
+    let completedCount = 0;
+    let totaltCount = this.state.todos.length;
+    for (let i = 0; i < this.state.todos.length; i++) {
+      if (this.state.todos[i].checked) {
+        completedCount++;
+      }
+    }
+    this.setState({
+      completedTasks: completedCount,
+      totalTasks: totaltCount
+    })
+  }
+
+  handleClickCheckbox (index) {
+    let tempTodos = this.state.todos.slice();
+    tempTodos[index].checked = !tempTodos[index].checked;
+    this.setState({
+      todos: tempTodos
+    })
+    this.updateTaskCount();
+  }
+
+  renderTodo(name, checked, index) {
+    return <Todo onClick={this.handleClickCheckbox.bind(this, index)} name={ name } checked={ checked } key={ index }/>
+  }
+
+  render() {
+    return (
+      <Container>
+        <Content contentContainerStyle={styles.content}>
+          <List>
+            { this.state.todos.map((item, key)=>(
+              this.renderTodo(item.name, item.checked, key))
+            )}
+          </List>
+        </Content>
+        <Footer >
+          <FooterTab style={styles.footer}>
+            <Button full>
+              <Text>Gruppe 37</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+export default createStackNavigator({
+    Activities: {
+      screen: ActivityScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Activities",
+        headerLeft: <Icon style={styles.menuIcon} name="menu" size={50} onPress={() => navigation.toggleDrawer() } />,
+        headerStyle: {
+          backgroundColor: "#645CFF",
+        },
+        headerTitleStyle: {
+          color: "white"
+        }
+      })
+    }
+  },
+  {
+    initialRouteName: 'Activities',
+  }
+);
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  menuIcon: {
+    padding: 15,
+    color: "white"
+  },
+footer: {
+  backgroundColor: "#645CFF",
+}
+})
