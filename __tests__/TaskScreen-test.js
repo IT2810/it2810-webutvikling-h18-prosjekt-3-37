dace411e0d7dd92b7311a1142ac926063f6031cb
Mikkel <mbnygard@stud.ntnu.no>
2018-10-19 15:14:38 +0200

    #9, #10 Merged with async

--- __tests__/TaskScreen-test.js
+++ __tests__/TaskScreen-test.js
@@ -1,9 +1,8 @@
 // __tests__/Login-page-test.js
 import 'react-native';
 import React from 'react';
-import ActivityScreen from '../screens/ActivityScreen';
-import {renderToDo} from '../screens/ActivityScreen';
-import Todo from '../components/Todo/Todo';
+import TaskScreen from '../screens/ActivityScreen';
+
 import renderer from 'react-test-renderer';
 
 it('works', () => {
@@ -13,12 +12,7 @@ it('works', () => {
 it('renders correctly', () => {
   jest.useFakeTimers();
   const tree = renderer.create(
-    <ActivityScreen />
+    <TaskScreen />
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
-/*
-test('renderTodo', () => {
-  expect(renderToDo('Walk',true,0)).toBe(<Todo name={'Walk'} checked={true} key={0}/>)
-});
-*/
