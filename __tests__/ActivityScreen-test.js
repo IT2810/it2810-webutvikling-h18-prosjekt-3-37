cb2a1b46b79a2606f57bb82532fa483b37734ac5
Mikkel <mbnygard@stud.ntnu.no>
2018-10-19 13:59:18 +0200

    #9 Added tests for TaskDetails #10 Added tests for ActivityScreen Ready for pulling from AsyncStorage

--- __tests__/ActivityScreen-test.js
+++ __tests__/ActivityScreen-test.js
@@ -1,23 +1,18 @@
 // __tests__/Login-page-test.js
 import 'react-native';
 import React from 'react';
-import TaskDetails from '../screens/TaskDetails';
+import ActivityScreen from '../screens/ActivityScreen';
+
 import renderer from 'react-test-renderer';
 
 it('works', () => {
   expect(true).toBeTruthy();
 });
 
-jest.mock('Platform', () => {
-  const Platform = require.requireActual('Platform');
-  Platform.OS = 'android';
-  return Platform;
-});
-
 it('renders correctly', () => {
   jest.useFakeTimers();
   const tree = renderer.create(
-    <TaskDetails />
+    <ActivityScreen />
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
